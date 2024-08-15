b765c4c7a604c66a19857ac4d58ac83021bba2d3
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-08-15 00:04:10 -0300

    Adicionando mais rotas do SoltaVoz

--- App/Route.php
+++ App/Route.php
@@ -14,10 +14,30 @@
                 'controller' => 'indexController',
                 'action' => 'index',
             );
-            $routes['about'] = array(
+            $routes['inscreverse'] = array(
                 'route' => '/inscreverse',
                 'controller' => 'indexController',
                 'action' => 'inscreverse',
+            );
+            $routes['registrar'] = array(
+                'route' => '/registrar',
+                'controller' => 'indexController',
+                'action' => 'registrar'
+            );
+            $routes['autenticar'] = array(
+                'route' => '/autenticar',
+                'controller' => 'AuthController',
+                'action' => 'autenticar'
+            );
+            $routes['timeline'] = array(
+                'route' => '/timeline',
+                'controller' => 'AppController',
+                'action' => 'timeline'
+            );
+            $routes['sair'] = array(
+                'route' => '/sair',
+                'controller' => 'AuthController',
+                'action' => 'sair' 
             );
 
             $this->setRoutes($routes);
